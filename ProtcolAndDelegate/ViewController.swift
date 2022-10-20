99d7ef8d3285ae89deb37c5186f24abf24760b47
Akanksha-H <aakanksha0499@gmail.com>
2022-10-20 08:09:39 +0530

    Initial Commit

---
+++ ProtcolAndDelegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProtcolAndDelegate
+//
+//  Created by Admin on 20/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
